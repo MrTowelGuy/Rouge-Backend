8d069a739d65fe809091d432be6000a4b546019c
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-29 09:44:57 -0400

    Databse Coonection

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ const morgan = require("morgan");
 
 // DATABASE CONNECTION
 ////////////////////////////////
+// Establish Connection
+mongoose.connect(MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
 
 // MiddleWare
 ////////////////////////////////
